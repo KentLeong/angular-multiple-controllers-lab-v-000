893eb8b81aa30221bfe54e2811e426df57466566
kentleong <kentleong@github.com>
2016-09-28 06:36:32 -0400

    Done.

---
+++ js/app/controllers/ContactController.js
@@ -0,0 +1,15 @@
+function ContactController() {
+  var vm = this;
+
+  vm.name = "Steve Jobs";
+  vm.email = "steve@apple.com"
+  vm.phone = "2129808765"
+
+  this.changeName = function() {
+    vm.name = "Tim Cook"
+  }
+}
+
+angular
+  .module('app')
+  .controller("ContactController", ContactController)
